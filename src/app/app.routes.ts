e18db117617b8986bb10bcf6f0b28e003867746d
Jeol28 <jorgeolaya2803@gmail.com>
2026-05-25 13:43:39 -0500

    CSAM

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingComponent } from './pages/landing/landing';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy';
 import { TermsComponent } from './pages/terms/terms';
 import { DeleteAccountComponent } from './pages/delete-account/delete-account';
+import { CsaeStandardsComponent } from './pages/csae-standards/csae-standards';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'terms', component: TermsComponent },
   { path: 'delete-account', component: DeleteAccountComponent },
+  { path: 'csae-standards', component: CsaeStandardsComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/csae-standards/csae-standards.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csae-standards',
+  imports: [],
+  templateUrl: './csae-standards.html',
+  styleUrl: './csae-standards.scss',
+})
+export class CsaeStandardsComponent {}
