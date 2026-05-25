6b07156704562621d5acd444d578bb4fe2488638
Jeol28 <jorgeolaya2803@gmail.com>
2026-05-25 15:13:57 -0500

    Links a google play

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
-export class LandingComponent {}
+export class LandingComponent {
+  readonly androidUrl = /android/i.test(navigator.userAgent)
+    ? 'https://play.google.com/store/apps/details?id=com.angarita.tuprofe'
+    : 'https://play.google.com/apps/testing/com.angarita.tuprofe';
+}
